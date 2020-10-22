f077beb82d9640cc5d337786a1b4801358313020
Gerald Roylance <GLRoylance@gmail.com>
2020-10-22 16:43:02 -0700

    reverse the left motor so forward works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demo.java
@@ -73,8 +73,8 @@ public class Demo extends OpMode
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        // leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        // rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Init demo done");
