633f82f865f154a72dd9987bfa769195a7f55563
anishapv <palaparthianisha0772@students.esuhsd.org>
2021-03-15 23:50:43 -0700

    add 2 shooter motors, DriveRaiderbot class for combining components

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRaiderbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRaiderbot.java
@@ -54,14 +54,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="HelloShooterAndDrive", group="SCHS Test")
+@TeleOp(name="DriveRaiderbot", group="SCHS Test")
 //@Disabled
-public class HelloShooter extends LinearOpMode {
+public class DriveRaiderbot extends LinearOpMode {
+
+    //This OpMode will be used to begin testing all components together (with driver control) as they are built
+    //Currently includes ability to drive robot chassis and shooter with 2 motors
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotorEx shooterMotor1 = null;
     private DcMotorEx shooterMotor2 = null;
+
+    private DcMotorEx leftDrive = null;
+    private DcMotorEx rightDrive = null;
 
     @Override
     public void runOpMode() {
@@ -74,10 +80,18 @@ public class HelloShooter extends LinearOpMode {
         shooterMotor1 = hardwareMap.get(DcMotorEx.class, "shooterMotor1");
         shooterMotor2 = hardwareMap.get(DcMotorEx.class, "shooterMotor2");
 
+        leftDrive  = hardwareMap.get(DcMotorEx.class, "leftMotor");
+        rightDrive = hardwareMap.get(DcMotorEx.class, "rightMotor");
+
         // Needs one side to be reversed and one side to be forward so that ring shoots out in correct direction
         // Wheels are on either side of ring, so reverse one motor to run backwards to allow ring to launch forward
         shooterMotor1.setDirection(DcMotor.Direction.FORWARD);
         shooterMotor2.setDirection(DcMotor.Direction.REVERSE);
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -86,16 +100,39 @@ public class HelloShooter extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            //Input velocity from gamepad joystick
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
             double velocity = gamepad2.left_stick_y * 650;
             velocity = Range.clip(velocity, -600, 600) ;
 
-            // Send calculated velocity to shooter motors
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
+
+            // Send calculated power to wheels
             shooterMotor1.setVelocity(velocity, AngleUnit.RADIANS);
             shooterMotor2.setVelocity(velocity, AngleUnit.RADIANS);
 
-            // Show the elapsed game time and shooter motor velocity (input and actual).
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            // Show the elapsed game time and wheel power and shooter motor velocity (input and actual)
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.addData("Status", "Motor velocity input (radians): " + velocity);
             //telemetry.addData("Status", "Motor velocity actual (radians): " + shooterMotor.getVelocity(AngleUnit.RADIANS));
             telemetry.addData("Status", "Shooter Motor 1 velocity actual (tick/sec): " + shooterMotor1.getVelocity());
